2229cceb9ec3d227a86b16498582b4a542cff6a5
Amari <amari.mecheri@gmail.com>
2023-01-08 08:51:51 +0100

    commit

--- src/Hooks/useHomeFetch.js
+++ src/Hooks/useHomeFetch.js
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 //API
 import API from "../API";
-// tslint:disable-next-line:no-unused-variable
 import { loadFromLocalStorage, saveToLocalStorage } from "../helpers";
 
 const initialState = {
